2ec5bd379782808e1f0ec8f0d37dce5c5116bf83
Russell Sabia <rjsabia@gmail.com>
2016-11-11 19:16:04 -0800

    backbone functions in place, not tested yet

--- js/app.js
+++ js/app.js
@@ -14,4 +14,52 @@ $(document).ready(function(){
 
 });
 
+var secretNum;
+var currentCount = 0;
 
+randomNumber();
+getUserGuess();
+
+
+/***********************************************************/
+function randomNumber(){
+
+	secretNum =  Math.floor((Math.random() * 100) + 1);
+
+	return secretNum;
+}
+/*********************************************************/
+ function getUserGuess() {
+       
+        $("#guessButton").click(guessGame);
+       
+        $("#userGuess").keydown(function (enter) {
+       
+            if (enter.keyCode == 13) {
+       
+                guessGame();
+            }
+        });
+    }
+ /******************************************************/
+ function guessGame(){
+
+ 	var guess = parseInt($('#userGuess').val());
+       
+       if (guess !== null && $.isNumeric(guess) 
+       	   && (0 < guess < 101)) { 
+
+       		currentCount += 1;
+
+       		$('#count').html(currentCount);
+
+       		if (guess === secretNum){
+
+       			$('#feedback').html('You Got it! The secret number is ' + secretNum);
+
+       		}
+
+       		else if ( guess )
+
+
+ }
