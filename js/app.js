3ddd08e23e253f94bda288203aa724c707e879f1
Russell Sabia <rjsabia@gmail.com>
2016-11-20 17:13:21 -0800

    final push for functionality, jQuery updated and newGame function

--- js/app.js
+++ js/app.js
@@ -1,52 +1,52 @@
 
 $(document).ready(function(){
+
+  // calls newGame to to start process, generate secret number
+  newGame();
 	
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+	$(".what").click(function(){
+  	$(".overlay").fadeIn(1000);
 
-  	});
+	});
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+	/*--- Hide information modal box ---*/
+	$("a.close").click(function(){
+		$(".overlay").fadeOut(1000);
+	});
+
+  //Listens for "Newgame button", then resets game
+  $(".new").click(function(){
+    
+    newGame();
+    // below is a way to reload from browser
+    // window.location.reload(false);
+  });
+
+  // listens for submit(button click to submit)
+  $('form').submit(function(event){
+    event.preventDefault();
+    
+    guess = document.getElementById("userGuess").value;
+    //converts input to integer 
+    parseInt(guess);
+    // calls checkGuess to verify input is correct
+    checkGuess(guess);
+    // clears input line for next guess
+    // jQuery way
+    $('#userGuess').val('');
+    // JS way
+    // document.getElementById('userGuess').value='';
+  });
 
 });
-//Below reloads from cache for new game//
-$(".new").click(function(){
-
-  window.location.reload(false);
-
-});
-
 //game variables
 var guess;
-var secretNum =  Math.floor((Math.random() * 100) + 1);
+var secretNum;
 var currentCount = 0;
 
-getUserGuess();
-
-
-//*****************************************************
-function getUserGuess(){
-
-document.getElementById("guessButton").addEventListener("click", function(){
-  
-  event.preventDefault();
-  
-  guess = document.getElementById("userGuess").value;
-
-  parseInt(guess);
-
-  checkGuess(guess);
-
-  document.getElementById('userGuess').value='';
-});
-
-}
-
 //******************************************************
+// checkGuess check if whole number and range
 function checkGuess(checkNum){
 
   if(guess % 1 != 0){
@@ -63,142 +63,123 @@ function checkGuess(checkNum){
   }
 
 }
-
 //****************************************************
+// runGame takes guess and processes it to check and see if you won
 function runGame(gameNum){
-      
-      if(Math.abs(secretNum - guess) == 0){
-      
-      document.getElementById("feedback").innerHTML = "You did it, you won!";
+  
+  if(Math.abs(secretNum - guess) == 0){
+    // Below is the jQuery way to output
+    $('#feedback').html('You did it, you won!');
+    // Below is the standard JS output
+    // document.getElementById("feedback").innerHTML = "You did it, you won!";
+    processGuess();
+  
+  } 
+  else if(Math.abs(secretNum - guess) <= 10){
+    // Below is the jQuery way to output
+    $('#feedback').html('Smokin Hot!!!');
+    // Below is the standard JS output
+    // document.getElementById("feedback").innerHTML = 'Smokin Hot!!!';
+    processGuess();
+  
+  } 
+  else if(Math.abs(secretNum - guess) <= 20 && Math.abs(secretNum - guess) > 10){
+    // Below is the jQuery way to output
+    $('#feedback').html('Your Hot!');
+    // Below is the standard JS output
+    // document.getElementById("feedback").innerHTML = 'Your Hot!';
+    processGuess();
+  
+  } 
+  else if(Math.abs(secretNum - guess) <= 30 && Math.abs(secretNum - guess) > 20){
+    //Below is the jQuery way to output
+    $('#feedback').html('Warming up'); 
+    //Below is the standard JS output
+    // document.getElementById("feedback").innerHTML = 'Warming up';
+    processGuess();
+  
+  } 
+  else if(Math.abs(secretNum - guess) <= 40 && Math.abs(secretNum - guess) > 30){
+    // Below is the jQuery way to output
+    $('#feedback').html('Your cold, come on man');
+    // Below is the standard JS output
+    // document.getElementById("feedback").innerHTML = 'Your cold, come on';
+    processGuess();
 
-      guessCounter();
-
-      guessPrint();
-
-      console.log(secretNum);
-      
-      } 
-      else if(Math.abs(secretNum - guess) <= 10){
-      
-       document.getElementById("feedback").innerHTML = 'Smokin Hot!!!';
-
-        guessCounter();
-
-        guessPrint();
-
-        console.log(secretNum);
-      
-      } 
-      else if(Math.abs(secretNum - guess) <= 20 && Math.abs(secretNum - guess) > 10){
-      
-        document.getElementById("feedback").innerHTML = 'Your Hot!';
-
-        guessCounter();
-
-        guessPrint();
-
-        console.log(secretNum);
-      
-      } 
-      else if(Math.abs(secretNum - guess) <= 30 && Math.abs(secretNum - guess) > 20){
-      
-        document.getElementById("feedback").innerHTML = 'Warming up';
-
-        guessCounter();
-
-        guessPrint();
-
-        console.log(secretNum);
-      
-      } 
-      else if(Math.abs(secretNum - guess) <= 40 && Math.abs(secretNum - guess) > 30){
-
-        document.getElementById("feedback").innerHTML = 'Your cold, come on';
-
-        guessCounter();
-
-        guessPrint();
-
-        console.log(secretNum);
-      }
-      else if(Math.abs(secretNum - guess) <= 50 && Math.abs(secretNum - guess) > 40){
-
-        document.getElementById("feedback").innerHTML = 'Ice cold man!';
-
-        guessCounter();
-
-        guessPrint();
-
-        console.log(secretNum);
-      }
-      else {
-      
-        document.getElementById("feedback").innerHTML = 'Subzero freezing';
-
-        guessCounter();
-
-        guessPrint();
-
-        console.log(secretNum);
-      }
-    }
-
-   //*************************************************************
-
-   function guessCounter(){
-      
-      currentCount++;
-      
-      document.getElementById("count").innerHTML = currentCount;
-    }
-//*****************************************************************
-
-function randomNumber(){
-
-  secretNum =  Math.floor((Math.random() * 100) + 1);
-
-  return secretNum;
+  }
+  else if(Math.abs(secretNum - guess) <= 50 && Math.abs(secretNum - guess) > 40){
+    // Below is the jQuery way to output
+    $('#feedback').html('Ice cold man!');
+    // Below is the standard JS output
+    // document.getElementById("feedback").innerHTML = 'Ice cold man!';
+    processGuess();
+ }
+  else {
+    
+    // Below is the jQuery way to output
+   $('#feedback').html('Subzero freezing');
+    // Below is the standard JS way to output
+    // document.getElementById("feedback").innerHTML = 'Subzero freezing';
+    processGuess();
+  }
 }
-//****************************************************************
+// *************************************************************
+// processGuess calls to run several other functions within loop
+function processGuess(){
 
+  guessCounter();
+
+  guessPrint();
+
+  console.log(secretNum);
+}
+
+//*************************************************************
+// guessCounter increments count of guesses and outputs it
+function guessCounter(){
+      
+  currentCount++;
+  // jQuery output
+  $('#count').html(currentCount);
+  //JS output 
+  // document.getElementById("count").innerHTML = currentCount;
+}
+//*****************************************************************
+// guessPrint creates new <li> and outputs each user guess
 function guessPrint(){
 
   $('#guessList').append('<li>' + guess + '</li>');
 }
+// *********************************************************
+// randomNum generates a random number between 1 & 100
+function randomNumber(){
 
-/***********************************************************/
-//Below are functions I am testing to make this work more effciently
-//*****************************************************************
-// function randomNumber(secretNum){
+  secretNum = Math.floor((Math.random() * 100) + 1);
 
-// 	secretNum =  Math.floor((Math.random() * 100) + 1);
+  return secretNum;
+}
+// *********************************************************
+// Clear Feedback line and returns to original message
+function clearFeedback(){
 
-// 	return secretNum;
-// }
+  $('#feedback').html('Make your Guess!');
+}
+// *********************************************************
+// Clears output from former game
+function clearList(){
 
-//********************************************************
-// function newGame(){
+  currentCount = 0;
+  $('#count').html('0'); 
+  $('#guessList').empty(); 
+  return currentCount;
+}
+//**********************************************************
+// when called, calls all other functions to clear for new game
+function newGame(){
 
-//   document.getElementById("new").addEventListener("click", function(){
-
-//     window.location.reload(false);
-//   }
-// }
-/*********************************************************/
-// function newGame(){
-      
-//       document.getElementById("guessButton").addEventListener("click", function(){
-//       clearVariables();
-//       randomNumber();
-//     }
-//     }
-// //**********************************************************
-// function clearVariables(){
-
-// 	currentCount = 0;
-// 	document.getElementById("feedback").innerHTML = 'Make your Guess!';
-// 	guess = '';
-// 	document.getElementById("guessList").innerHTML = '';
-// }
-// /******************************************************/
-
+  $('#userGuess').val('');
+  clearFeedback();
+  randomNumber();
+  clearList();
+}
+// ***********************************************************
